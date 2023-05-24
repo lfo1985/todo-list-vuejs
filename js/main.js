a28e1162ac7c05e46f678e36221fa43b2dafac8c
lfo1985 <lfo1985@gmail.com>
2023-05-23 21:16:31 -0300

    Ralização de ajustes de layout e funcionalidade; melhorias de código; aplicação do localstorage

--- js/main.js
+++ js/main.js
@@ -1,50 +1,71 @@
+var tarefa = {
+    status: false,
+    edit: false,
+    disabled: false,
+    focus: true,
+    exibeErroEdicao: false
+};
+
 const app = {
     data(){
         return {
             criadas: 0,
             concluidas: 0,
             exibeErro: false,
-            exibeErroEdicao: false,
-            tarefas: window.tarefas,
+            tarefas: [],
             novaTarefa: {},
             editaTarefa: {}
         };
     },
     methods: {
         getIndex: function(tarefa){
-            return this.tarefas.findIndex(function(item){
-                return item.id == tarefa.id;
-            });
+            return this.tarefas.findIndex(item => item.id == tarefa.id);
+        },
+        setId: function(){
+            return {id: Math.floor(Math.random() * 10000000000) + 1};
         },
         setErro: function(exibe){
             this.exibeErro = exibe;
         },
-        totalConcluidas: function(){
-            this.concluidas = tarefas.filter(function(item){
-                return item.status == true;
-            }).length;
-            return this.concluidas;
+        setErroEdicao: function(tarefa, exibe){
+            tarefa.exibeErroEdicao = exibe;
         },
-        barraProgresso: function(){
-            return this.tarefas.length > 0 ? parseInt((this.totalConcluidas() / this.tarefas.length) * 100) : 0;
+        setFocus: function(tarefa, focus){
+            tarefa.focus = focus;
+        },
+        setEdit: function(tarefa, edit){
+            tarefa.edit = edit;
+        },
+        setDadosLocalStorage: function(){
+            /**
+             * Utiliza JSON.strigfy para enviar uma string para o localStorage
+             */
+            localStorage.setItem('tarefas', JSON.stringify(this.tarefas));
+        },
+        getDadosLocalStorage: function(){
+            /**
+             * Utiliza JSON.parse para converter a string em JSON
+             */
+            return localStorage.getItem('tarefas') ? JSON.parse(localStorage.getItem('tarefas')) : [];
+        },
+        getTotalConcluidas: function(){
+            return this.tarefas.filter(item => item.status == true ).length;
+        },
+        getBarraProgresso: function(){
+            return this.tarefas.length > 0 ? parseInt((this.getTotalConcluidas() / this.tarefas.length) * 100) : 0;
         },
         statusBotoes: function(status){
-            this.tarefas.forEach(item => {
-                item.disabled = status;
-            });
+            this.tarefas.forEach(item => item.disabled = status );
         },
         adicionar: function(){
             if(this.novaTarefa.titulo){
-                this.tarefas.push({
-                    id: this.tarefas.length + 1,
-                    titulo: this.novaTarefa.titulo,
-                    status: false,
-                    edit: false,
-                    disabled: false,
-                    focus: true
-                });
-                this.limpar();
+                var cloneTarefa = {...tarefa};
+                Object.assign(cloneTarefa, this.setId());
+                Object.assign(cloneTarefa, this.novaTarefa);
+                this.tarefas.push(cloneTarefa);
+                this.resetNovaTarefa();
                 this.setErro(false);
+                this.setDadosLocalStorage();
             } else {
                 this.setErro(true);
             }
@@ -52,40 +73,56 @@ const app = {
         apagar: function(tarefa){
             if(confirm('Deseja apagar esta tarefa?')){
                 this.tarefas.splice(this.getIndex(tarefa), 1);
+                this.setDadosLocalStorage();
+            }
+        },
+        apagarTodasTarefas: function(){
+            if(confirm('Deseja apagar TODAS as tarefas?')){
+                this.tarefas = [];
+                this.setDadosLocalStorage();
             }
         },
         editar: function(tarefa){
             this.statusBotoes(true);
-            tarefa.edit = true;
-            tarefa.focus = true;
+            this.setEdit(tarefa, true);
+            this.setFocus(tarefa, true);
             this.editaTarefa.titulo = tarefa.titulo;
+            this.setDadosLocalStorage();
         },
         cancelarEdicao: function(tarefa){
-            tarefa.edit = false;
-            tarefa.exibeErroEdicao = false;
             this.statusBotoes(false);
-            this.editaTarefa = {};
+            this.setEdit(tarefa, false);
+            this.setErroEdicao(tarefa, false);
+            this.resetEditarTarefa();
+            this.setDadosLocalStorage();
         },
         atualizar: function(tarefa){
             if(this.editaTarefa.titulo){
-                tarefa.edit = false;
-                tarefa.titulo = this.editaTarefa.titulo;
-                this.editaTarefa = {};
-                tarefa.exibeErroEdicao = false;
-                tarefa.focus = false;
                 this.statusBotoes(false);
+                this.setErroEdicao(tarefa, false);
+                this.setEdit(tarefa, false);
+                this.setFocus(tarefa, false);
+                tarefa.titulo = this.editaTarefa.titulo;
+                this.resetEditarTarefa();
             } else {
-                tarefa.focus = true;
-                tarefa.exibeErroEdicao = true;
+                this.setFocus(tarefa, true);
+                this.setErroEdicao(tarefa, true);
             }
+            this.setDadosLocalStorage();
         },
-        limpar: function(){
+        alteraStatus: function(){
+            this.setDadosLocalStorage();
+        },
+        resetNovaTarefa: function(){
             this.novaTarefa = {};
         },
-        limparTodos: function(){
-            this.tarefas = [];
+        resetEditarTarefa: function(){
+            this.editaTarefa = {};
         }
-    }
+    },
+    created() {
+        this.tarefas = this.getDadosLocalStorage();
+    },
 };
 
 var appVue = Vue.createApp(app);
@@ -95,26 +132,5 @@ appVue.directive('focus', (el, binding) => {
         el.focus();
     }
 });
-
-// appVue.directive('focus', {
-//     mounted(el, binding) {
-//         if(binding.value == true){
-//             el.focus();
-//         }
-//     },
-// });
-
-// appVue.directive('focus-in', {
-//     updated(el, binding) {
-//         if(binding.value == true){
-//             el.focus();
-//         }
-//     },
-//     created(el, binding) {
-//         if(binding.value == true){
-//             el.focus();
-//         }
-//     },
-// });
 
 appVue.mount('#root');
